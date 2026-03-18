430b0b4e213fd08a28f331dddf40a95e618aab2d
23501ap <23501ap@hvhs.school.nz>
2026-03-18 21:56:45 +0000

    i

--- FLYBIRDS.js
+++ FLYBIRDS.js
@@ -1,5 +1,22 @@
+
 function setup() {
 
+
+    let cnv;
+let circle;
+class Circle {
+    constructor(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    display() {
+        fill('white');
+        circle(this.x, this.y, this.r * 2);
+    }
+}
+console.log("Hello World");
+cnv = new createCanvas(600, 550);
 
 
 }
